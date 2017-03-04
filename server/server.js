939837c7961bf9cca6de500e1624ed3443b6003f
ivan.khoteev <ivan.khoteev@saltpepper.ru>
2017-03-04 14:35:33 +0300

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text,
   });
+
   todo.save().then(result => {
     res.send(result);
   }, err => {
@@ -31,3 +32,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+module.exports = app;
