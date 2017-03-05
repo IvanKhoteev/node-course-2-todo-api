d7660c381520493d964c20eb9fac4f21f13654f8
ivan.khoteev <ivan.khoteev@saltpepper.ru>
2017-03-05 11:41:51 +0300

    Setup heroku configuration

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+const db = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(db);
 
 module.exports = {mongoose};
