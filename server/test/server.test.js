3e7cf4c7e118d9299356cb067dee72a0039b8700
ivan.khoteev <ivan.khoteev@saltpepper.ru>
2017-03-14 22:42:22 +0300

    Add POST /users/login route and test for it

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -294,3 +294,34 @@ describe('GET /users/me', () => {
       .end(done);
   });
 });
+
+describe('POST /users/login', () => {
+  it('should return 400 if invalid data', done => {
+    const email = users[0].email;
+    const password = '12345';
+
+    request(app)
+      .post('/users/login')
+      .send({email, password})
+      .expect(400)
+      .expect(res => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+
+  it('should return user if data valid', done => {
+    const email = users[0].email;
+    const password = users[0].password;
+
+    request(app)
+      .post('/users/login')
+      .send({email, password})
+      .expect(200)
+      .expect(res => {
+        expect(res.body.email).toBe(users[0].email);
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+      })
+      .end(done);
+  });
+});
